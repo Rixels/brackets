ac0aca6698fd5e255d34c3e7abb06f1198b36db2
Rixels <mikey.drozd@gmail.com>
2024-08-26 15:29:33 +0300

    feat: solutuion

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let per = bracketsConfig.join('').replace(/,/g, '');
+      let val = [];
+  for (let bracket of str) {
+let perIndex = per.indexOf(bracket)
+ if (perIndex % 2 === 0) {   
+if (bracket === per[perIndex + 1] && val[val.length - 1] === perIndex){
+ val.pop();
+ } else if (bracket === per[perIndex + 1] && val[val.length - 1] !== perIndex) {
+  val.push(perIndex)}
+  else{
+  val.push(perIndex)}
+  } 
+ else {
+ if (val.pop() !== perIndex-1){
+   return false;}
+  }    
 }
+ return val.length === 0 
+ }
